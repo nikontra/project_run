1c94f2331f55d2f7f9667ab3360633494c01305f
nikontra <nikontra@yandex.ru>
2025-08-29 07:21:12 +0300

    add company_details

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -114,3 +114,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPANY_NAME = 'Жизнь это бег!'
+SLOGAN = 'Если встречный ветер усиливается, значит, ты набираешь скорость!'
+CONTACTS = 'Город Задунайск, улица 30 Лет СССР, дом 30'
